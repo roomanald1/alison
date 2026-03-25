e490b21db7aefaeccd8c85db29182f3be21c89fb
Ronnie <ronnie.day@hqb-markets.com>
2026-03-25 19:27:55 +0000

    a

--- website/src/Banner.tsx
+++ website/src/Banner.tsx
@@ -1,30 +1,28 @@
 import type { JSX } from "react/jsx-runtime";
 
-export function Banner(element: JSX.Element)
-{
+export function Banner(element: JSX.Element) {
   return (<div
-  style={{
-    position: "relative",
-    width: "100vw",
-    height: "360px",
-    marginBottom: "40px",
-   background: "linear-gradient(90deg, #f2f3f4a2 0%, #b4d9f167 20%, #9731ce21 100%)",
-  }}
->
-
-  <div
     style={{
-      position: "absolute",
-      top: "50%",
-      left: "50%",
-      transform: "translate(-50%, -50%)",
-      textAlign: "center",
-      color: "#333",
-      width: "90vw"
-
+      position: "relative",
+      width: "100vw",
+      height: "360px",
+      marginBottom: "40px",
+      background: "linear-gradient(90deg, #64a19c23 0%, #b4d9f165 20%,  #9731ce2c 70%,  #9731ce4d 100%)",
     }}
   >
-    {element}
-  </div>
-</div>)
+
+    <div
+      style={{
+        position: "absolute",
+        top: "50%",
+        left: "50%",
+        transform: "translate(-50%, -50%)",
+        textAlign: "center",
+
+        width: "90vw"
+      }}
+    >
+      {element}
+    </div>
+  </div>)
 }
